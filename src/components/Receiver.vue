1a4a91a8c18a69bf150a02bebabe97cd3c588ba4
Bruno Lapierre <BrunoL@MiraGeoscience.local>
2022-02-09 15:47:05 -0500

    color picker test

--- src/components/Receiver.vue
+++ src/components/Receiver.vue
@@ -6,11 +6,60 @@
       {{ message }}
     </p>
 
-<!--    <button v-on:click="togglePopup">clique</button>-->
+    <!--    <button v-on:click="togglePopup">clique</button>-->
 
-    <a v-on:click="togglePopup" href="#">clic here</a>
+<!--    <a v-on:click="togglePopup" href="#">clic here</a>-->
 
-    <p v-show="popUp">Hello oui ca marche !!!</p>
+<!--    <label for="couleur">{{showColor}}</label>-->
+<!--    <input  id="couleur" type="text">-->
+
+    <div>
+<!--      <label for="add-color-item">color : {{showColor}}</label>-->
+<!--      <input v-model="color" id="add-color-item" type="text" />-->
+<!--      <input id="add-color-item" type="text" />-->
+      <input v-model="color" type="text" placeholder="vfvfvfbhhhh" />
+
+      <a v-on:click="togglePopup" href="#">clic here</a> |
+      <a v-on:click="clearColor" href="#">clear</a>
+    </div>
+
+    <!--    <p v-show="popUp">Hello oui ca marche !!!</p>-->
+
+    <v-container v-show="popUp">
+      <v-row>
+        <v-col
+            cols="12"
+            md="4"
+        >
+          <v-btn
+              v-for="t in types"
+              :key="t"
+              class="my-4"
+              block
+              @click="type = t"
+          >
+            {{ t }}
+          </v-btn>
+        </v-col>
+        <v-col
+            class="d-flex justify-center"
+        >
+          <v-color-picker v-model="color"></v-color-picker>
+        </v-col>
+        <v-col
+            cols="12"
+            md="4"
+        >
+          <v-sheet
+              dark
+              class="pa-4"
+          >
+            <pre>{{ showColor }}</pre>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+
   </div>
 </template>
 
@@ -30,8 +79,35 @@ export default {
   // },
   data: () => ({
     message: 'Pas de message',
-    popUp: false
+    popUp: false,
+
+    types: ['hex', 'hexa', 'rgba', 'hsla', 'hsva'],
+    type: 'hex',
+    hex: '#FFFFFF',
+    hexa: '#FF00FFFF',
+    rgba: { r: 255, g: 0, b: 255, a: 1 },
+    hsla: { h: 300, s: 1, l: 0.5, a: 1 },
+    hsva: { h: 300, s: 1, v: 1, a: 1 },
   }),
+
+  computed: {
+    color: {
+      get () {
+        return this[this.type]
+      },
+      set (v) {
+        this[this.type] = v
+      },
+    },
+    showColor () {
+      if (typeof this.color === 'string') return this.color
+
+      return JSON.stringify(Object.keys(this.color).reduce((color, key) => {
+        color[key] = Number(this.color[key].toFixed(2))
+        return color
+      }, {}), null, 2)
+    },
+  },
 
   methods: {
     updateMessage(payload) {
@@ -41,6 +117,11 @@ export default {
 
     togglePopup() {
       this.popUp = !this.popUp
+    },
+
+    clearColor() {
+      this.hex = '#FFFFFF'
+      // this.color.set('')
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <HelloWorld />
-    <Receiver />
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-// import HelloWorld from "./components/HelloWorld";
-import Receiver from '@/components/Receiver.vue';
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld, Receiver
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
